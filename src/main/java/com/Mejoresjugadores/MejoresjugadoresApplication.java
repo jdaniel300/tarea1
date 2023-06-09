3f9087432cf6baf51da2066419635a6f1413633c
PC <PC@192.168.0.18>
2023-06-08 23:13:19 -0600

    Tarea 1

---
+++ src/main/java/com/Mejoresjugadores/MejoresjugadoresApplication.java
@@ -0,0 +1,13 @@
+package com.Mejoresjugadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MejoresjugadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MejoresjugadoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Mejoresjugadores/MejoresjugadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mejoresjugadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MejoresjugadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
